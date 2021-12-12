6181035a95017f6a71c4e53de8e0cd7991eb45e4
Md-Monowarul-Amin <95131180+Md-Monowarul-Amin@users.noreply.github.com>
2021-12-12 22:09:13 +0600

    MAIN

---
+++ LAB 04/ROOM/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ROOM_CLASS_LIBRARY;
+
+namespace ROOM
+{
+    
+    public partial class Form1 : Form
+    {
+        List<single_room> list_single_room = new List<single_room>();
+        List<double_room> list_double_room = new List<double_room>();   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ADD_SINGLE_ROOM_Click(object sender, EventArgs e)
+        {
+            string object_name = textBox1.Text;
+            single_room dummy_room = new single_room();
+            dummy_room.name = object_name;
+            dummy_room.qty =  int.Parse(textBox2.Text);
+            dummy_room.air_condition = textBox3.Text;
+            dummy_room.gaming_setup = textBox4.Text;
+            list_single_room.Add(dummy_room);
+
+            MessageBox.Show("you have to pay "+ dummy_room.Calculate_cost() + "per month");
+
+            
+        }
+
+        private void singleQty_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double_room dummy_double_room = new double_room();
+            dummy_double_room.name = textBox8.Text;
+            dummy_double_room.qty = int.Parse(textBox7.Text);
+            dummy_double_room.air_condition= textBox6.Text;
+            dummy_double_room.extra_tv = textBox5.Text;
+            dummy_double_room.complementaray_breakfast = textBox9.Text;
+            list_double_room.Add(dummy_double_room);
+
+            MessageBox.Show("you have to pay" + dummy_double_room.Calculate_cost() + "per month");
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool search = true;
+            foreach(single_room dummy_room in list_single_room)
+            {
+                if (dummy_room.name == textBox10.Text)
+                {
+                    int room_availability;
+                    room_availability = dummy_room.qty - int.Parse(textBox11.Text);
+                    if (room_availability < 0)
+                    {
+                        MessageBox.Show("Room number Out of limit");
+                    }
+                    else
+                    {
+                        dummy_room.qty = room_availability;
+                        int cal = dummy_room.Calculate_cost() * int.Parse(textBox11.Text);  
+                        MessageBox.Show("there exists" + room_availability + " more rooms");
+                        MessageBox.Show("you have to pay" + cal + "per month");
+                    }
+                    search = false;
+                    break;
+
+                }
+                
+            }
+            if (search)
+            {
+                foreach(double_room dummy_room in list_double_room)
+                {
+                    if (dummy_room.name == textBox10.Text)
+                    {
+                        int room_availability;
+                        room_availability = dummy_room.qty - int.Parse(textBox11.Text);
+                        if (room_availability < 0)
+                        {
+                            MessageBox.Show("Room number Out of limit");
+                        }
+                        else
+                        {
+                            dummy_room.qty = room_availability;
+                            int cal = dummy_room.Calculate_cost() * int.Parse(textBox11.Text);
+                            MessageBox.Show("there exists" + room_availability + "room");
+                            MessageBox.Show("you have to pay" + cal + "per month");
+                        }
+
+                    }
+                }
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string room_name = textBox12.Text;
+            bool search = true;
+            foreach(single_room room in list_single_room)
+            {
+                if (room.name == room_name)
+                {
+                    ROOM_HISTORY.Items.Add(room.name);
+                    ROOM_HISTORY.Items.Add("available rooms:" + room.qty);
+                    if (room.air_condition == "1")
+                    {
+                        ROOM_HISTORY.Items.Add("AC available");
+                    }
+                    else
+                    {
+                        ROOM_HISTORY.Items.Add("AC not available");
+                    }
+                    if (room.gaming_setup == "1")
+                    {
+                        ROOM_HISTORY.Items.Add("gaming available");
+                    }
+                    else
+                    {
+                        ROOM_HISTORY.Items.Add("gaming not available");
+                    }
+                    search = false;
+                    break;
+
+                }
+            }
+            foreach(double_room room in list_double_room)
+            {
+                if(room.name == room_name)
+                {
+                    ROOM_HISTORY.Items.Add(room.name);
+                    ROOM_HISTORY.Items.Add("available rooms:" + room.qty);
+                    if (room.air_condition == "1")
+                    {
+                        ROOM_HISTORY.Items.Add("AC available");
+                    }
+                    else
+                    {
+                        ROOM_HISTORY.Items.Add("AC not available");
+                    }
+                    
+                    ROOM_HISTORY.Items.Add("TV: "+ room.TV);
+                }
+            }
+        }
+    }
+}
---
+++ LAB 04/ROOM_CLASS_LIBRARY/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROOM_CLASS_LIBRARY
+{
+    public class ROOM
+    {
+        public string name;
+        public int TV = 1;
+        public int refrigerator = 1;
+        public int qty;
+        public string air_condition;
+
+    }
+    public class single_room : ROOM
+    {
+        public int bed = 1;
+        public string gaming_setup;
+        public int base_cost = 2000;
+        public string Calculate_cost()
+        {
+            int cost = base_cost;
+            if (air_condition == "1")
+            {
+                cost = cost + 500;
+            }
+            if (gaming_setup == "1")
+            {
+                cost = cost + 1000;
+            }
+            string cost_str = cost.ToString();
+            return cost_str;
+        }
+
+    }
+    public class double_room : ROOM
+    {
+        public int bed = 2;
+        public int base_cost = 3500;
+        public string extra_tv;
+        public string complementaray_breakfast;
+
+        public int Calculate_cost()
+        {
+            int cost = base_cost;
+            if (air_condition == "1")
+            {
+                cost = cost + 500;
+            }
+            if (extra_tv == "1")
+            {
+                cost += 1000;
+            }
+            if (complementaray_breakfast == "1")
+            {
+                cost = cost + 500;
+            }
+            return cost;
+        }
+
+    }
+}
+
+
