e9e52d867caba699ae6bb1ca833fae961801650b
Md-Monowarul-Amin <95131180+Md-Monowarul-Amin@users.noreply.github.com>
2022-03-22 19:26:57 +0600

    Lab final added

---
+++ LAB FINAL/LabFinal/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using LabFinalLibrary;
+
+namespace LabFinal
+{
+    public partial class Form1 : Form
+    {   
+        List<Student> student_list = new List<Student>();
+        private object reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (var reader = new StreamReader(@"C:\Users\USER\Desktop\LAB_OOC_1\LabFinal\SWE4201MarkSheet_NEW.csv"))
+
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+
+                }
+
+                string output_string_2 = "";
+
+                foreach (string str in listA)
+                {
+
+                    string[] str_sep = str.Split(',');
+                    string id = str_sep[0];
+                    string name = str_sep[1];
+                    string attendance_str = str_sep[2];
+                    int attendance = Convert.ToInt32(attendance_str);
+                    int quiz_1_mark;
+                    int quiz_2_mark;
+                    int quiz_3_mark;
+                    int quiz_4_mark;
+                    int mid_mark;
+                    int final_mark;
+                    int viva_mark;
+                    if(str_sep[3] == null)
+                    {
+                        quiz_1_mark = 0;
+                    }
+                    else
+                    {
+                        quiz_1_mark = Convert.ToInt32(str_sep[3]);
+                    }
+                    
+                    if(str_sep[4] == null)
+                    {
+                        quiz_2_mark = 0;
+                    }
+                    else
+                    {
+                        quiz_2_mark = Convert.ToInt32(str_sep[4]);
+                    }
+                    
+                    if(str_sep[5] == null)
+                    {
+                        quiz_3_mark = 0;
+                    }
+                    else
+                    {
+                        quiz_3_mark = Convert.ToInt32(str_sep[5]);
+                    }
+                    
+                    if(str_sep[6] == null)
+                    {
+                        quiz_4_mark = 0;
+                    }
+                    else
+                    {
+                        quiz_4_mark = Convert.ToInt32(str_sep[6]);
+                    }
+
+                    if (str_sep[7] == null)
+                    {
+                        mid_mark = 0;
+                    }
+                    else
+                    {
+                        mid_mark = Convert.ToInt32(str_sep[7]);
+                    }
+                    if(str_sep[8] == null)
+                    {
+                        final_mark = 0;
+                    }
+                    else
+                    {
+                        final_mark = Convert.ToInt32(str_sep[8]);
+                    }
+                    if(str_sep[9] == null)
+                    {
+                        viva_mark = 0;
+                    }
+                    else
+                    {
+                        viva_mark = Convert.ToInt32(str_sep[9]);
+                    }
+                    
+
+                    Student dummy_student = new Student(id, name, attendance, quiz_1_mark, quiz_2_mark, quiz_3_mark, quiz_4_mark, mid_mark, final_mark, viva_mark);
+                    student_list.Add(dummy_student);
+                    int total = dummy_student.total;
+                    
+                    
+                    float percent = total / 3;
+                    dummy_student.percentage = percent;
+                    string grade = dummy_student.count_grade();
+
+                    output_string_2 += str_sep[0] + " " + str_sep[1] + " " + percent.ToString() + "%" + grade;
+                    listBox1.Items.Add(output_string_2);
+                    output_string_2 = "";
+                }
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            foreach(Student student in student_list)
+            {
+                if(student.Id == id)
+                {
+                    label26.Text = student.attendance_mark.ToString();
+                    label24.Text = student.quiz_1_mark.ToString();
+                    label22.Text = student.quiz_2_mark.ToString();
+                    label21.Text = student.quiz_3_mark.ToString();
+                    label23.Text = student.quiz_4_mark.ToString();
+                 
+                    label20.Text = student.mid_mark.ToString();
+                    label19.Text = student.final_mark.ToString();
+                    label18.Text = student.viva_mark.ToString();
+                    label17.Text = student.total.ToString();
+                    label17.Text = student.percentage.ToString() + "%";
+                    label16.Text = student.grade.ToString();
+                    label27.Text = student.total.ToString();
+
+                }
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label28_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label27_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LAB FINAL/LabFinalLibrary/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFinalLibrary
+{
+    public class Student
+    {
+        public string Id;
+        public string Name { get; set; }
+        public int attendance_mark = 0;
+        public int quiz_1_mark = 0;
+        public int quiz_2_mark = 0;
+        public int quiz_3_mark = 0;
+        public int quiz_4_mark = 0;
+        public int quiz_total = 0;
+        public int mid_mark = 0;
+        public int final_mark = 0;
+        public int viva_mark = 0;
+        public int total = 0;
+        public float percentage = 0;
+        public string grade = "F";
+
+
+        public Student(string id, string name, int attendance_mark, int quiz_1_mark, int quiz_2_mark, int quiz_3_mark, int quiz_4_mark, int mid_mark, int final_mark, int viva_mark)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.quiz_1_mark = quiz_1_mark;
+            this.quiz_2_mark = quiz_2_mark;
+            this.quiz_3_mark = quiz_3_mark;
+            this.quiz_4_mark = quiz_4_mark;
+            this.attendance_mark = attendance_mark;
+            this.quiz_total = quiz_1_mark + quiz_2_mark + quiz_3_mark + quiz_4_mark;
+            this.final_mark = final_mark;
+            this.mid_mark = mid_mark;
+            this.viva_mark = viva_mark;
+            this.total = quiz_total + mid_mark + final_mark + viva_mark + attendance_mark;
+            float percentage_ff = (this.total / 300) * 100;
+            this.percentage = percentage_ff;
+        }
+        public string count_grade()
+        {
+
+            
+
+            if (this.percentage >= 80 && this.percentage <= 100)
+            {
+                this.grade = "A+";
+            }
+            else if (this.percentage >= 75 && this.percentage <= 79)
+            {
+                this.grade = "A";
+            }
+            else if (this.percentage >= 70 && this.percentage <= 74)
+            {
+                this.grade = "A-";
+            }
+            else if (this.percentage >= 65 && this.percentage <= 69)
+            {
+                this.grade = "B+";
+            }
+            else if (this.percentage >= 60 && this.percentage <= 64)
+            {
+                this.grade = "B";
+            }
+            else if (this.percentage >= 55 && this.percentage <= 59)
+            {
+                this.grade = "B-";
+            }
+            else if (this.percentage >= 50 && this.percentage <= 54)
+            {
+                this.grade = "C+";
+            }
+            else if (this.percentage >= 45 && this.percentage <= 49)
+            {
+                this.grade = "C";
+            }
+            else if (this.percentage >= 40 && this.percentage <= 44)
+            {
+                this.grade = "D";
+            }
+            else if (this.percentage >= 0 && this.percentage <= 39)
+            {
+                this.grade = "F";
+            }
+
+            else
+            {
+                this.grade = "Unknown Grade";
+            }
+
+            return this.grade;
+
+        }
+
+    }
+}
